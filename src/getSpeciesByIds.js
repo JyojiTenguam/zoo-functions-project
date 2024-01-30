b7811c47863fb19ff858eb0d7a8974fa2a8b23d2
JyojiTenguam <j.y.oji@hotmail.com>
2024-01-30 01:47:17 -0300

    Resolvendo requisito 01 de Project Zoo Functions

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (!ids || ids.length === 0) {
+    return [];
+  }
+
+  const result = data.species.filter((species) => ids.includes(species.id));
+  return result;
 };
 
 module.exports = getSpeciesByIds;
