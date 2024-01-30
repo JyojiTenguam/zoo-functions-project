f88b08572adf3495131b3e9c517ba316e67d7518
JyojiTenguam <j.y.oji@hotmail.com>
2024-01-30 02:08:59 -0300

    Resolvendo requisito 02 de Project Zoo Functions

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const species = data.species.find((specie) => specie.name === animal);
+
+  if (!species) {
+    // Se a espécie não for encontrada, consideramos que não há animais dessa espécie
+    return false;
+  }
+
+  // Verificamos se todos os animais têm a idade mínima especificada
+  return species.residents.every((animals) => animals.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
