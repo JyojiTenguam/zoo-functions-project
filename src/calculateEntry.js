6f109b88c64c029abb2113c1c2a3ea175aa8827a
JyojiTenguam <j.y.oji@hotmail.com>
2024-01-31 04:13:38 -0300

    Resolvendo requisito 08 de Project Zoo Functions

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => entrants.reduce((counts, person) => {
+  const { age } = person;
+
+  const newCounts = { ...counts };
+
+  if (age < 18) {
+    newCounts.child += 1;
+  } else if (age >= 18 && age < 50) {
+    newCounts.adult += 1;
+  } else if (age >= 50) {
+    newCounts.senior += 1;
+  }
+
+  return newCounts;
+}, { child: 0, adult: 0, senior: 0 });
 
 const calculateEntry = (entrants) => {
-  // seu código aquib
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const { child = 0, adult = 0, senior = 0 } = countEntrants(entrants) || {};
+  const { prices } = data;
+  return child * prices.child + adult * prices.adult + senior * prices.senior;
 };
 
 module.exports = { calculateEntry, countEntrants };
