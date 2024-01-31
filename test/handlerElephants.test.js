2f48f338bc2155cebdf53e3e1c5b03be18273713
JyojiTenguam <j.y.oji@hotmail.com>
2024-01-31 01:54:48 -0300

    Resolvendo requisito 06 e 07 de Project Zoo Functions

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,38 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função handlerElephants', () => {
+  it('Deve retornar a quantidade de elefantes', () => {
+    const result = handlerElephants('count');
+    expect(result).toBe(4); // Adaptar conforme a lógica real dos seus dados
+  });
+
+  it('Deve retornar um array com nomes de elefantes', () => {
+    const result = handlerElephants('names');
+    expect(result).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']); // Adaptar conforme a lógica real dos seus dados
+  });
+
+  it('Deve retornar a média de idade dos elefantes', () => {
+    const result = handlerElephants('averageAge');
+    expect(result).toBeCloseTo(10.5); // Adaptar conforme a lógica real dos seus dados
+  });
+
+  it('Deve retornar a localização dos elefantes dentro do Zoológico', () => {
+    const result = handlerElephants('location');
+    expect(result).toEqual('NW'); // Adaptar conforme a lógica real dos seus dados
+  });
+
+  it('Deve retornar a popularidade dos elefantes', () => {
+    const result = handlerElephants('popularity');
+    expect(result).toBe(5); // Adaptar conforme a lógica real dos seus dados
+  });
+
+  it('Deve retornar um array com a relação de dias em que é possível visitar os elefantes', () => {
+    const result = handlerElephants('availability');
+    expect(result).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']); // Adaptar conforme a lógica real dos seus dados
+  });
+
+  it('Deve retornar undefined ao não passar argumentos', () => {
+    const result = handlerElephants();
+    expect(result).toBeUndefined();
+  });
+});
