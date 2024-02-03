106642d4811460b36788c5bb1d5ec6664a876be2
JyojiTenguam <j.y.oji@hotmail.com>
2024-02-03 18:59:25 -0300

    Resolvendo requisito 09 de Project Zoo Functions

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const geralZoo = () => {
+  const objeto = daysOfWeek.reduce((acc, elem) => {
+    acc[elem] = {
+      officeHour: `Open from ${data.hours[elem].open}am until ${data.hours[elem].close}pm`,
+      exhibition: data.species
+        .filter((animal) => animal.availability.includes(elem))
+        .map((a) => a.name),
+    };
+    return acc;
+  }, {});
+
+  objeto.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return objeto;
+};
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (data.species.some((animal) => animal.name === scheduleTarget)) {
+    const foundAnimal = data.species.find((animal) => animal.name === scheduleTarget);
+    return foundAnimal.availability;
+  }
+  if (scheduleTarget === undefined || !daysOfWeek.includes(scheduleTarget)) {
+    return geralZoo();
+  }
+  if (daysOfWeek.includes(scheduleTarget)) {
+    return { [scheduleTarget]: geralZoo()[scheduleTarget] };
+  }
 };
 
 module.exports = getSchedule;
