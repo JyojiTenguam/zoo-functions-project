90169021d8f319c5ae6fc6cd412df98374e5de64
JyojiTenguam <j.y.oji@hotmail.com>
2024-02-03 19:17:37 -0300

    Resolvendo requisito 10 de Project Zoo Functions

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
-};
+function getOldestFromFirstSpecies(id) {
+  const idPrimeiroAnimal = data.employees.find((func) => func.id === id).responsibleFor[0];
+  const animalObj = data.species.find((specie) => specie.id === idPrimeiroAnimal).residents;  
+  return Object.values(animalObj.sort((a, b) => a.age - b.age)[animalObj.length - 1]);
+}
 
 module.exports = getOldestFromFirstSpecies;
