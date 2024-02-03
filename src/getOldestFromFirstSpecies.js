b080d6d8447127731c1d7822e2b22e1b81c34410
JyojiTenguam <j.y.oji@hotmail.com>
2024-02-03 19:57:00 -0300

    Resolvendo problemas do lint do requisito 10 de Project Zoo Functions

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const idPrimeiroAnimal = data.employees.find((func) => func.id === id).responsibleFor[0];
-  const animalObj = data.species.find((specie) => specie.id === idPrimeiroAnimal).residents;  
+  const animalObj = data.species.find((specie) => specie.id === idPrimeiroAnimal).residents;
   return Object.values(animalObj.sort((a, b) => a.age - b.age)[animalObj.length - 1]);
 }
 
