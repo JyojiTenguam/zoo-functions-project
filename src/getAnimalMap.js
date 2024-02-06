a41d906bc9e9d6375fae81bcfb3f2555c6a0d0f0
JyojiTenguam <j.y.oji@hotmail.com>
2024-02-05 21:33:00 -0300

    Resolvendo requisito 14 e corrigindo requisito 13 de Project Zoo Functions

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,37 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
-};
+function getAnimalNames(location, includeNames, sorted, sex) {
+  return species
+    .filter((animal) => animal.location === location)
+    .map((animal) => {
+      const residents = sex ? animal.residents
+        .filter((resident) => resident.sex === sex) : animal.residents;
+
+      if (includeNames) {
+        const dataToReturn = {
+          [animal.name]: residents.map((resident) => resident.name),
+        };
+
+        if (sorted) {
+          dataToReturn[animal.name] = dataToReturn[animal.name].sort();
+        }
+
+        return dataToReturn;
+      }
+
+      return animal.name;
+    });
+}
+
+function getAnimalMap(options = {}) {
+  const { includeNames, sorted, sex } = options;
+
+  const animalMap = ['NE', 'NW', 'SE', 'SW'].reduce((acc, location) => {
+    acc[location] = getAnimalNames(location, includeNames, sorted, sex);
+    return acc;
+  }, {});
+
+  return animalMap;
+}
 
 module.exports = getAnimalMap;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,31 +1,48 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
-  it('Deverá retornar  "The zoo is closed" para Monday e 09:00-AM', () => {
+  it('Deverá retornar o objeto completo para nenhum argumento fornecido', () => {
+    const result = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(result).toEqual(expected);
+  });
+
+  it('Deverá retornar "The zoo is closed" para Monday e 09:00-AM', () => {
     const result = getOpeningHours('Monday', '09:00-AM');
     expect(result).toBe('The zoo is closed');
   });
-  it('Deverá retornar  "The zoo is closed" para Tuesday e 09:00-AM', () => {
+
+  it('Deverá retornar "The zoo is open" para Tuesday e 09:00-AM', () => {
     const result = getOpeningHours('Tuesday', '09:00-AM');
     expect(result).toBe('The zoo is open');
   });
-  it('Deverá retornar  "The zoo is closed" para Wednesday e 09:00-AM', () => {
+
+  it('Deverá retornar "The zoo is closed" para Wednesday e 09:00-PM', () => {
     const result = getOpeningHours('Wednesday', '09:00-PM');
     expect(result).toBe('The zoo is closed');
   });
-  it('Deve lançar uma exceção para Thu and 09:00-AM com a mensagem "The day must be valid."', () => {
-    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError('The day must be valid.');
+
+  it('Deve lançar uma exceção para Thu e 09:00-AM com a mensagem "The day must be valid. Example: Monday"', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError('The day must be valid. Example: Monday');
   });
 
-  it('Deve lançar uma exceção para Friday and 09:00-ZM com a mensagem "The abbreviation must be \'AM\' or \'PM\'"', () => {
+  it('Deve lançar uma exceção para Friday e 09:00-ZM com a mensagem "The abbreviation must be \'AM\' or \'PM\'"', () => {
     expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrowError('The abbreviation must be \'AM\' or \'PM\'');
   });
 
-  it('Deve lançar uma exceção para Saturday and C9:00-AM com a mensagem "The hour should represent a number"', () => {
+  it('Deve lançar uma exceção para Saturday e C9:00-AM com a mensagem "The hour should represent a number"', () => {
     expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrowError('The hour should represent a number');
   });
 
-  it('Deve lançar uma exceção para Sunday and 09:c0-AM com a mensagem "The minutes should represent a number"', () => {
+  it('Deve lançar uma exceção para Sunday e 09:c0-AM com a mensagem "The minutes should represent a number"', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrowError('The minutes should represent a number');
   });
 });
